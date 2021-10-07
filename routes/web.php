d52a3e49d653c9dbd3d158b5673b07c50e548080
Hewlett Packard <danielbima07@gmail.com>
2021-10-07 21:12:21 +0700

    Praktikum6_F_0112_GD

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FacultyController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('faculty',FacultyController::class);
-
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('faculties', FacultyController::class);
